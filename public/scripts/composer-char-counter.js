e7a97ede89cbd2ec9a59bfa9c7e25727da2ab605
mandatoryrecess <rossanneclamp@gmail.com>
2021-06-16 00:35:04 +0000

    added historical tweet css and html

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,12 @@
-$(document).ready(function() {
- 
-
-  $('#tweet-text').on("input", function() {
-   
-    const charLeft =  140 - $(this).val().length;
-    const counter = $('.counter')
+$(document).ready(function () {
+  $("#tweet-text").on("input", function () {
+    const charLeft = 140 - $(this).val().length;
+    const counter = $(".counter");
     counter.val(charLeft);
     if (charLeft < 0) {
-      counter.css('color', 'red');
+      counter.css("color", "red");
     } else {
-      counter.css('color', 'black');
-    };
-  
-  })
-  
-  
+      counter.css("color", "black");
+    }
   });
+});
